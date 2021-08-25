017fb00dc4e27ff381836aae3db636f8564173cb
Ramkumar C <kumarramkumar2000@gmail.com>
2021-08-25 18:34:35 +0530

    Commit

--- src/main/java/com/enrollment/repository/TimeTableRepository.java
+++ src/main/java/com/enrollment/repository/TimeTableRepository.java
@@ -1,6 +1,7 @@
 package com.enrollment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -10,6 +11,7 @@ import com.enrollment.entity.TimeTableEntity;
 @Transactional
 @Repository
 public interface TimeTableRepository extends JpaRepository<TimeTableEntity,Long>{
+	@Modifying
 	@Query("DELETE FROM TimeTableEntity t WHERE t.semester.id=:semId AND t.department.deptId=:deptId AND t.day=:day")
 	void deleteByIdAndDay(@Param("semId") Long semId,@Param("deptId") Long deptId, @Param("day") String day);
 	
